Implement:  - GIN for edge classification  - AMLworld information message. Fix:  - AMLworld normalization Ongoing:  - Reverse message passing

pyg/custom_dataset/AMLworld.py
@@ -153,9 +153,14 @@ class AddEgoIds(BaseTransform):
         return data
 
 
+def z_norm(data):
+    std = data.std(0)
+    std = torch.where(std == 0, torch.tensor(1, dtype=torch.float32).cpu(), std)
+    return (data - data.mean(0)) / std
+
 class AMLworld(InMemoryDataset):
 
-    def __init__(self, root, opt="HI-Small", transform=None, pre_transform=None, pre_fileter=None, force_download=False, verbose=True):
+    def __init__(self, root, opt="HI-Small", transform=None, pre_transform=None, pre_fileter=None, force_download=False, verbose=True, *args, **kwargs):
         """
         Args:
             opt (str, optional): _description_. Defaults to "HI-Small".
@@ -163,6 +168,7 @@ class AMLworld(InMemoryDataset):
 
         self.force_download = force_download
         self.verbose = verbose
+        self.processed_in_this_call = False
 
         all_options = ["HI-Small", "HI-Medium", "HI-Large",
                        "LI-Small", "LI-Medium", "LI-Large"]
@@ -173,9 +179,13 @@ class AMLworld(InMemoryDataset):
 
         self.opt = opt
 
-        super().__init__(root, transform, pre_transform, pre_fileter)
+        super().__init__(root, transform, pre_transform, pre_fileter, *args, **kwargs)
 
         self.load(self.processed_paths[0])
+        
+        if not self.processed_in_this_call and self.verbose:
+            from loguru import logger
+            logger.info(self._data.information)
 
     @property
     def raw_file_names(self):
@@ -216,6 +226,9 @@ class AMLworld(InMemoryDataset):
                 "Downloading this dataset requires a configured Kaggle API for Python. Please refer to https://github.com/Kaggle/kaggle-api for proper configuration!")
 
     def process(self):
+        self.processed_in_this_call = True
+        infor_str_list = []
+        
         if self.verbose:
             from loguru import logger
 
@@ -306,6 +319,12 @@ class AMLworld(InMemoryDataset):
         # Edge: transaction lable
         y = torch.LongTensor(df_edges['Is Laundering'].to_numpy())
 
+        infor_str_list.append(
+            f"Illicit ratio = {sum(y)} / {len(y)} = {sum(y) / len(y) * 100:.2f}%")
+        infor_str_list.append(
+            f"Number of nodes (holdings doing transcations) = {df_nodes.shape[0]}")
+        infor_str_list.append(
+            f"Number of transactions = {df_edges.shape[0]}")
         if self.verbose:
             logger.info(
                 f"Illicit ratio = {sum(y)} / {len(y)} = {sum(y) / len(y) * 100:.2f}%")
@@ -318,11 +337,6 @@ class AMLworld(InMemoryDataset):
                          'Received Currency', 'Payment Format']
         node_features = ['Feature']
 
-        if self.verbose:
-            logger.info(f'Edge features being used: {edge_features}')
-            logger.info(
-                f'Node features being used: {node_features} ("Feature" is a placeholder feature of all 1s)')
-
         x = torch.tensor(df_nodes.loc[:, node_features].to_numpy()).float()
         edge_index = torch.LongTensor(
             df_edges.loc[:, ['from_id', 'to_id']].to_numpy().T)
@@ -331,6 +345,8 @@ class AMLworld(InMemoryDataset):
 
         n_days = int(timestamps.max() / (3600 * 24) + 1)
         n_samples = y.shape[0]
+        infor_str_list.append(
+            f'number of days and transactions in the data: {n_days} days, {n_samples} transactions')
         if self.verbose:
             logger.info(
                 f'number of days and transactions in the data: {n_days} days, {n_samples} transactions')
@@ -384,6 +400,12 @@ class AMLworld(InMemoryDataset):
         val_inds = torch.cat(split_inds[1])
         te_inds = torch.cat(split_inds[2])
 
+        infor_str_list.append(f"Total train samples: {tr_inds.shape[0] / y.shape[0] * 100 :.2f}% || IR: "
+                                   f"{y[tr_inds].float().mean() * 100 :.2f}% || Train days: {split[0][:5]}")
+        infor_str_list.append(f"Total val samples: {val_inds.shape[0] / y.shape[0] * 100 :.2f}% || IR: "
+                                   f"{y[val_inds].float().mean() * 100:.2f}% || Val days: {split[1][:5]}")
+        infor_str_list.append(f"Total test samples: {te_inds.shape[0] / y.shape[0] * 100 :.2f}% || IR: "
+                                   f"{y[te_inds].float().mean() * 100:.2f}% || Test days: {split[2][:5]}")
         if self.verbose:
             logger.info(f"Total train samples: {tr_inds.shape[0] / y.shape[0] * 100 :.2f}% || IR: "
                         f"{y[tr_inds].float().mean() * 100 :.2f}% || Train days: {split[0][:5]}")
@@ -443,6 +465,7 @@ class AMLworld(InMemoryDataset):
         tr_data.add_ports()
         val_data.add_ports()
         te_data.add_ports()
+        edge_features.extend(["In-Port", "Out-Port"])
         if self.verbose:
             logger.info(f"Done: adding ports")
 
@@ -451,8 +474,22 @@ class AMLworld(InMemoryDataset):
         tr_data.add_time_deltas()
         val_data.add_time_deltas()
         te_data.add_time_deltas()
+        edge_features.extend(["In-TimeDelta", "Out-TimeDelta"])
         if self.verbose:
             logger.info(f"Done: adding time-deltas")
+
+        edge_features_colID = {}
+        for id, feature in enumerate(edge_features):
+            edge_features_colID[feature] = id
+
+        infor_str_list.append(
+            f'Edge features being used: {edge_features}')
+        infor_str_list.append(
+            f'Node features being used: {node_features} ("Feature" is a placeholder feature of all 1s)')
+        if self.verbose:
+            logger.info(f'Edge features being used: {edge_features}')
+            logger.info(
+                f'Node features being used: {node_features} ("Feature" is a placeholder feature of all 1s)')
 
         tmp_edge_attr = torch.zeros(
             (edge_attr.shape[0], tr_data.edge_attr.shape[-1]))
@@ -477,6 +514,17 @@ class AMLworld(InMemoryDataset):
             test_mask=test_mask,
             timestamps=timestamps,
         )
+        
+        # Normalize the edge attribute
+        norm_col = []
+        for feature, id in edge_features_colID.items():
+            if feature != "Payment Format":
+                norm_col.append(id)
+        data.edge_attr[:, norm_col] = z_norm(data.edge_attr[:, norm_col])
+        
+
+        data.edge_features_colID = edge_features_colID
+        data.information = "\n".join(infor_str_list)
 
         data_list = [data]
 
@@ -490,6 +538,11 @@ class AMLworld(InMemoryDataset):
 
         # TODO: Process pattern tag into data object
         raw_pattern_file = os.path.join(self.raw_dir, self.opt+"_Patterns.txt")
+        
+    
+    
+
+
 
 
 def main():

pyg/model/GAT.py
@@ -19,7 +19,7 @@ class GAT_PyG(GAT_Base):
 class GAT_Custom(torch.nn.Module):
     def __init__(self,
                  in_channels,
-                 hidden_node_channels_per_head: int | list[int],
+                 hidden_channels_per_head: int | list[int],
                  num_layers: int, out_channels: int,
                  heads: int | list[int] = 8,
                  output_heads: int = 1,
@@ -37,10 +37,16 @@ class GAT_Custom(torch.nn.Module):
             Conv = GATv2Conv
         else:
             Conv = GATConv
+            
+        self.in_channels = in_channels
+        self.hidden_channels_per_head = hidden_channels_per_head
+        self.out_channels = out_channels
+        self.num_layers = num_layers
+        self.dropout = dropout
 
-        if type(hidden_node_channels_per_head) == int:
-            hidden_node_channels_per_head = [
-                hidden_node_channels_per_head] * (num_layers-1)
+        if type(hidden_channels_per_head) == int:
+            hidden_channels_per_head = [
+                hidden_channels_per_head] * (num_layers-1)
 
         if type(heads) == int:
             heads = [heads] * (num_layers-1)
@@ -48,18 +54,15 @@ class GAT_Custom(torch.nn.Module):
         self.conv = ModuleList()
         self.conv.append(
             Conv(in_channels,
-                 hidden_node_channels_per_head[0], heads[0], dropout=dropout)
+                 hidden_channels_per_head[0], heads[0], dropout=dropout)
         )
         for i in range(1, num_layers-1):
             self.conv.append(
-                Conv(hidden_node_channels_per_head[i-1]*heads[i-1],
-                     hidden_node_channels_per_head[i], heads[i], dropout=dropout)
+                Conv(hidden_channels_per_head[i-1]*heads[i-1],
+                     hidden_channels_per_head[i], heads[i], dropout=dropout)
             )
         self.conv.append(Conv(
-            hidden_node_channels_per_head[-1] * heads[-1], out_channels, output_heads, concat=False, dropout=dropout))
-
-        self.dropout = dropout
-        self.num_layers = num_layers
+            hidden_channels_per_head[-1] * heads[-1], out_channels, output_heads, concat=False, dropout=dropout))
 
         self.jk_mode = jk
         if not self.jk_mode in ["cat", None]:
@@ -69,24 +72,24 @@ class GAT_Custom(torch.nn.Module):
             self.jk = JumpingKnowledge(jk)
             jk_in_channels = out_channels
             for i in range(len(heads)):
-                jk_in_channels += heads[i] * hidden_node_channels_per_head[i]
+                jk_in_channels += heads[i] * hidden_channels_per_head[i]
             self.jk_linear = Linear(jk_in_channels, out_channels)
             
         self.skip_connection = skip_connection
         if self.skip_connection:
             self.skip_proj = ModuleList()
             self.skip_proj.append(
-                self.get_skip_proj(in_channels, hidden_node_channels_per_head[0]*heads[0])
+                self.get_skip_proj(in_channels, hidden_channels_per_head[0]*heads[0])
             )
             for i in range(1, num_layers-1):
                 self.skip_proj.append(
                     self.get_skip_proj(
-                        hidden_node_channels_per_head[i-1]*heads[i-1],
-                        hidden_node_channels_per_head[i]*heads[i]
+                        hidden_channels_per_head[i-1]*heads[i-1],
+                        hidden_channels_per_head[i]*heads[i]
                     )
                 )
             self.skip_proj.append(
-                self.get_skip_proj(hidden_node_channels_per_head[-1]*heads[-1], out_channels)
+                self.get_skip_proj(hidden_channels_per_head[-1]*heads[-1], out_channels)
             )
             
 
@@ -100,9 +103,7 @@ class GAT_Custom(torch.nn.Module):
         for conv in self.conv:
             conv.reset_parameters()
         if self.jk_mode:
-            for nn in self.jk_linear:
-                if isinstance(nn, Linear):
-                    nn.reset_parameters()
+            self.jk_linear.reset_parameters()
         if self.skip_connection:
             for nn in self.skip_proj:
                 if isinstance(nn, Linear):

pyg/model/GIN.py
@@ -3,11 +3,11 @@ import torch.nn.functional as F
 
 from loguru import logger
 
-from torch.nn import Linear, Identity, ModuleList
+from torch.nn import Linear, Identity, ModuleList, Sequential, ReLU, Dropout
 
 from torch_geometric.nn.models import GIN as GIN_Base
 from torch_geometric.nn.models import JumpingKnowledge, MLP
-from torch_geometric.nn import GINConv, GINEConv
+from torch_geometric.nn import GINConv, GINEConv, BatchNorm
 
 
 class GIN_PyG(GIN_Base):
@@ -18,9 +18,10 @@ class GIN_PyG(GIN_Base):
 
 class GIN_Custom(torch.nn.Module):
     def __init__(self,
-                 in_channels,
-                 hidden_node_channels: int | list[int],
-                 num_layers: int, out_channels: int,
+                 in_channels: int,
+                 hidden_channels: int | list[int],
+                 num_layers: int,
+                 out_channels: int,
                  num_MLP_layers: int = 2,
                  GINE: bool = False,
                  dropout: float = 0.6,
@@ -31,40 +32,42 @@ class GIN_Custom(torch.nn.Module):
                  **kwargs):
         super().__init__()
         self.config = config
-        
+
         if GINE:
-            Conv = GINEConv
+            self.Conv = GINEConv
         else:
-            Conv = GINConv
-        
-        self.dropout = dropout
+            self.Conv = GINConv
+
+        self.in_channels = in_channels
+        self.hidden_channels = hidden_channels
+        self.out_channels = out_channels
         self.num_layers = num_layers
-        
-        if type(hidden_node_channels) == int:
-            hidden_node_channels = [hidden_node_channels] * (num_layers-1)
+        self.dropout = dropout
+
+        if type(hidden_channels) == int:
+            hidden_channels = [hidden_channels] * (num_layers-1)
 
         self.conv = ModuleList()
         self.conv.append(
-            Conv(nn=self.init_MLP(
-                in_channels, 
-                hidden_node_channels[0], 
+            self.Conv(nn=self.init_MLP_for_GIN(
+                in_channels,
+                hidden_channels[0],
                 num_MLP_layers
             ))
         )
         for i in range(1, num_layers-1):
             self.conv.append(
-                Conv(self.init_MLP(
-                    hidden_node_channels[i-1], 
-                    hidden_node_channels[i], 
+                self.Conv(self.init_MLP_for_GIN(
+                    hidden_channels[i-1],
+                    hidden_channels[i],
                     num_MLP_layers
                 ))
             )
-        self.conv.append(Conv(self.init_MLP(
-                    hidden_node_channels[-1], 
-                    out_channels, 
-                    num_MLP_layers
+        self.conv.append(self.Conv(self.init_MLP_for_GIN(
+            hidden_channels[-1],
+            out_channels,
+            num_MLP_layers
         )))
-
 
         self.jk_mode = jk
         if not self.jk_mode in ["cat", None]:
@@ -73,34 +76,34 @@ class GIN_Custom(torch.nn.Module):
         if self.jk_mode != None:
             self.jk = JumpingKnowledge(jk)
             jk_in_channels = out_channels
-            for i in range(len(hidden_node_channels)):
-                jk_in_channels += hidden_node_channels[i]
+            for i in range(len(hidden_channels)):
+                jk_in_channels += hidden_channels[i]
             self.jk_linear = Linear(jk_in_channels, out_channels)
-            
+
         self.skip_connection = skip_connection
         if self.skip_connection:
             self.skip_proj = ModuleList()
             self.skip_proj.append(
-                self.get_skip_proj(in_channels, hidden_node_channels[0])
+                self.get_skip_proj(in_channels, hidden_channels[0])
             )
             for i in range(1, num_layers-1):
                 self.skip_proj.append(
                     self.get_skip_proj(
-                        hidden_node_channels[i-1],
-                        hidden_node_channels[i]
+                        hidden_channels[i-1],
+                        hidden_channels[i]
                     )
                 )
             self.skip_proj.append(
-                self.get_skip_proj(hidden_node_channels[-1], out_channels)
+                self.get_skip_proj(hidden_channels[-1], out_channels)
             )
-            
-    def init_MLP(self, in_channels: int, out_channels: int, num_MLP_layers):
+
+    def init_MLP_for_GIN(self, in_channels: int, out_channels: int, num_MLP_layers):
         channel_list = [in_channels]
         for i in range(num_MLP_layers):
             channel_list.append(out_channels)
-        mlp = MLP(channel_list, dropout=self.dropout, norm=None)
+        mlp = MLP(channel_list, norm=None)
         return mlp
-    
+
     def get_skip_proj(self, in_channels, out_channels):
         if in_channels == out_channels:
             return Identity()
@@ -111,9 +114,7 @@ class GIN_Custom(torch.nn.Module):
         for conv in self.conv:
             conv.reset_parameters()
         if self.jk_mode:
-            for nn in self.jk_linear:
-                if isinstance(nn, Linear):
-                    nn.reset_parameters()
+            self.jk_linear.reset_parameters()
         if self.skip_connection:
             for nn in self.skip_proj:
                 if isinstance(nn, Linear):
@@ -130,8 +131,95 @@ class GIN_Custom(torch.nn.Module):
             x = F.elu(x)
             if self.jk_mode != None:
                 xs.append(x)
-                
+
         if self.jk_mode != None:
             x = self.jk(xs)
             x = self.jk_linear(x)
         return x
+
+
+class GINe(GIN_Custom):
+    # Adjusted model architecture from https://github.com/IBM/Multi-GNN/blob/252b0252afca109d1d216c411c59ff70753b25fc/models.py#L7
+    def __init__(self,
+                 in_channels: int,
+                 hidden_channels: int,
+                 out_channels: int,
+                 edge_updates: bool = False,
+                 edge_dim=None,
+                 batch_norm=True,
+                 *args,
+                 **kwargs):
+        super().__init__(
+            in_channels=hidden_channels, 
+            hidden_channels=hidden_channels,
+            out_channels=hidden_channels, GINE=True, *args, **kwargs)
+        
+        self.batch_norm = batch_norm 
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        
+        self.node_emb = Linear(self.in_channels, self.hidden_channels)
+        self.edge_emb = Linear(edge_dim, self.hidden_channels) 
+        self.edge_updates = edge_updates
+
+
+        self.convs = ModuleList()
+        self.emlps = ModuleList()
+        self.batch_norms = ModuleList()
+        for _ in range(self.num_layers):
+            conv = self.Conv(
+                nn=self.init_MLP_for_GIN(hidden_channels, hidden_channels, 2),
+                edge_dim=self.hidden_channels
+            )
+            if self.edge_updates:
+                self.emlps.append(
+                        self.init_MLP_for_GIN(3 * hidden_channels, hidden_channels, 2)
+                )
+            self.convs.append(conv)
+            if self.batch_norm:
+                self.batch_norms.append(BatchNorm(self.hidden_channels))
+
+        self.mlp = Sequential(
+            Linear(self.hidden_channels*3, 50),
+            ReLU(),
+            Dropout(self.dropout),
+            Linear(50, 25),
+            ReLU(),
+            Dropout(self.dropout),
+            Linear(25, self.out_channels)
+        )
+
+    def forward(self, x, edge_index, edge_attr):
+        src, dst = edge_index
+
+        x = self.node_emb(x)
+        edge_attr = self.edge_emb(edge_attr)
+
+        xs = []
+        for i in range(self.num_layers):
+            # x = F.dropout(x, p=self.dropout, training=self.training)
+            if self.skip_connection:
+                residual = self.skip_proj[i](x)
+            x = self.conv[i](x, edge_index, edge_attr)
+            x = self.batch_norms[i](x) if self.batch_norm else x
+            x = x + residual if self.skip_connection else x
+            x = F.relu(x)
+            if self.jk_mode != None:
+                xs.append(x)
+                
+            if self.edge_updates:
+                if self.skip_connection:
+                    residual = self.skip_proj[i](edge_attr)
+                edge_attr = self.emlps[i](torch.cat([x[src], x[dst], edge_attr], -1))
+                edge_attr = edge_attr + residual
+                
+        out = torch.cat([x[src], x[dst], edge_attr], -1)
+        # x = torch.cat([x[src], x[dst], edge_attr], -1).relu() # Dont known whether the relu is useful or not
+        
+        # Original:
+        # x = x[edge_index.T].reshape(-1, 2 * self.hidden_channels).relu()
+        # x = torch.cat((x, edge_attr.view(-1, edge_attr.shape[1])), 1)
+        
+        out = self.mlp(out)
+
+        return out

pyg/model/PNA.py
@@ -28,7 +28,7 @@ class PNA_PyG(PNA_Base):
 class PNA_Custom(torch.nn.Module):
     def __init__(self,
                  in_channels,
-                 hidden_node_channels: int | list[int],
+                 hidden_channels: int | list[int],
                  num_layers: int, out_channels: int,
                  deg,
                  aggregators=['mean', 'min', 'max', 'std'],
@@ -44,30 +44,33 @@ class PNA_Custom(torch.nn.Module):
 
         Conv = PNAConv
 
-        self.dropout = dropout
+        self.in_channels = in_channels
+        self.hidden_channels = hidden_channels
+        self.out_channels = out_channels
         self.num_layers = num_layers
+        self.dropout = dropout
 
-        if type(hidden_node_channels) == int:
-            hidden_node_channels = [hidden_node_channels] * (num_layers-1)
+        if type(hidden_channels) == int:
+            hidden_channels = [hidden_channels] * (num_layers-1)
 
         self.conv = ModuleList()
         self.conv.append(
             Conv(in_channels,
-                 hidden_node_channels[0],
+                 hidden_channels[0],
                  aggregators,
                  scalers,
                  deg=deg
                  ))
         for i in range(1, num_layers-1):
             self.conv.append(
-                Conv(hidden_node_channels[i-1],
-                     hidden_node_channels[i],
+                Conv(hidden_channels[i-1],
+                     hidden_channels[i],
                      aggregators,
                      scalers,
                      deg=deg
                      ))
         self.conv.append(
-            Conv(hidden_node_channels[-1],
+            Conv(hidden_channels[-1],
                  out_channels,
                  aggregators,
                  scalers,
@@ -81,25 +84,25 @@ class PNA_Custom(torch.nn.Module):
         if self.jk_mode != None:
             self.jk = JumpingKnowledge(jk)
             jk_in_channels = out_channels
-            for i in range(len(hidden_node_channels)):
-                jk_in_channels += hidden_node_channels[i]
+            for i in range(len(hidden_channels)):
+                jk_in_channels += hidden_channels[i]
             self.jk_linear = Linear(jk_in_channels, out_channels)
 
         self.skip_connection = skip_connection
         if self.skip_connection:
             self.skip_proj = ModuleList()
             self.skip_proj.append(
-                self.get_skip_proj(in_channels, hidden_node_channels[0])
+                self.get_skip_proj(in_channels, hidden_channels[0])
             )
             for i in range(1, num_layers-1):
                 self.skip_proj.append(
                     self.get_skip_proj(
-                        hidden_node_channels[i-1],
-                        hidden_node_channels[i]
+                        hidden_channels[i-1],
+                        hidden_channels[i]
                     )
                 )
             self.skip_proj.append(
-                self.get_skip_proj(hidden_node_channels[-1], out_channels)
+                self.get_skip_proj(hidden_channels[-1], out_channels)
             )
 
     def get_skip_proj(self, in_channels, out_channels):
@@ -112,9 +115,7 @@ class PNA_Custom(torch.nn.Module):
         for conv in self.conv:
             conv.reset_parameters()
         if self.jk_mode:
-            for nn in self.jk_linear:
-                if isinstance(nn, Linear):
-                    nn.reset_parameters()
+            self.jk_linear.reset_parameters()
         if self.skip_connection:
             for nn in self.skip_proj:
                 if isinstance(nn, Linear):

pyg/train_utils.py
@@ -9,20 +9,36 @@ from sklearn.metrics import f1_score
 from sklearn.utils.class_weight import compute_class_weight
 
 
+def get_sample_input(loader, task_type, device):
+    sample_input = {}
+    sample_batch = next(iter(loader))
+    sample_input["x"] = sample_batch.x.to(device)
+    sample_input["edge_index"] = sample_batch.edge_index.to(device)
+    if task_type.endswith("EC"):
+        sample_input["edge_attr"] = sample_batch.edge_attr
+    return sample_input
+
+
 def get_pos_weight_for_BCEWithLogitsLoss(data):
     # TODO: get weights for graph batching
     total_num = data.num_nodes
     pos_cnt = torch.unique(data.y, return_counts=True)[-1]
     neg_cnt = total_num - pos_cnt
-    return neg_cnt/pos_cnt
+    pos_weight = neg_cnt/pos_cnt
+    logger.info(f"Loss weight: pos_weight={pos_weight}")
+    return pos_weight
 
 
-def get_weight_for_CrossEntropyLoss(data):
-    # TODO: get weights for graph batching
-    y = data.y.numpy()
-    weight = compute_class_weight(
-        class_weight="balanced", classes=np.unique(y), y=y)
-    weight = torch.tensor(weight)
+def get_weight_for_CrossEntropyLoss(data, config):
+    weight = config["hyperparameters"].get("CE_weight", None)
+    if weight == None:
+        # TODO: get weights for graph batching
+        y = data.y.numpy()
+        weight = compute_class_weight(
+            class_weight="balanced", classes=np.unique(y), y=y)
+    weight = torch.tensor(weight, dtype=torch.float32)
+    logger.info(f"Loss weight: weight={weight}")
+    return weight
 
 
 def get_loss_fn(config, loader, reduction="mean"):
@@ -30,13 +46,14 @@ def get_loss_fn(config, loader, reduction="mean"):
     if config["general_config"]["sampling_strategy"] != "GraphBatching":
         data = loader.data
     else:
-        logger.warning("Weighted loss function is not implemented for graph batching!")
+        logger.warning(
+            "Weighted loss function is not implemented for graph batching!")
         if config["hyperparameters"]["weighted_CE"] or config["hyperparameters"]["weighted_BCE"]:
             raise NotImplementedError
-    
+
     if dataset_config["task_type"] == "single-label-NC":
         if config["hyperparameters"]["weighted_CE"]:
-            weight = get_weight_for_CrossEntropyLoss(data)
+            weight = get_weight_for_CrossEntropyLoss(data, config)
         else:
             weight = None
         return torch.nn.CrossEntropyLoss(weight=weight, reduction=reduction)
@@ -50,7 +67,7 @@ def get_loss_fn(config, loader, reduction="mean"):
 
     elif dataset_config["task_type"] == "single-label-EC":
         if config["hyperparameters"]["weighted_CE"]:
-            weight = get_weight_for_CrossEntropyLoss(data)
+            weight = get_weight_for_CrossEntropyLoss(data, config)
         else:
             weight = None
         return torch.nn.CrossEntropyLoss(weight=weight, reduction=reduction)
@@ -163,7 +180,8 @@ def edge_classification_step(mode: str, epoch, loader, model, loss_fn, optimizer
     avg_loss = total_loss/total_num
     mlflow.log_metric(f"{mode} loss", avg_loss, epoch)
 
-    f1 = f1_score(truths, predictions, average="micro")
+    # Note that 1 is the minority class
+    f1 = f1_score(truths, predictions)
     mlflow.log_metric(f"{mode} F1", f1, epoch)
 
     return avg_loss, f1, predictions, truths